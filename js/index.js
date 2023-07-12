7c1892a5a033296f02d281a6c26d116c2dc0cfc0
Carlos Hernández Flórez <carloshflorez.web@gmail.com>
2023-07-12 14:53:48 +0200

    Ajustes varios
    En los logos, en las reviews

--- js/index.js
+++ js/index.js
@@ -6,3 +6,14 @@
 $(`.Header-btn`).click(function () {
   $(`.Header-responsive`).toggleClass(`isActive`);
 });
+
+//SCRIPT PARA CARGAR LOS VÍDEOS DE LAS REVIEWS
+
+$(`.Reviews-li`).click(function () {
+  $(`.Reviews-img`).removeClass(`isActive`);
+  let i = $(`.Reviews-li`).index($(this));
+  let title = $(`.Reviews-li`).index($(this));
+  let source = $(`.Reviews-li`).eq(i).attr(`src`);
+  $(`.Reviews-img`).eq(i).addClass(`isActive`);
+  $(`.Reviews-slider`).attr(`src`, source);
+});
