c3eea709f7c1e24cc1df7546080bdef298edba36
Carlos Hernández Flórez <carloshflorez.web@gmail.com>
2023-07-13 10:28:50 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -2,7 +2,6 @@
 
 //SCRIPT PARA EL BOTON HAMBURGUESA EN EL MODO MÓVIL Y TABLET
 
-//Al hacer click en Header-btn hacemos toggle de la clase isActive para la etiqueta Header-responsive
 $(`.Header-btn`).click(function () {
   $(`.Header-responsive`).toggleClass(`isActive`);
 });
